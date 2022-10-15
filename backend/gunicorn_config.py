e7805cad31c24d26584d14098af56752cf4aba5b
Prateek61 <prateekpoudel61@gmail.com>
2022-10-15 19:54:37 +0545

    implimented backend jwt authetication

---
+++ backend/gunicorn_config.py
@@ -0,0 +1,4 @@
+import multiprocessing
+
+bind = "0.0.0.0:5080"
+workers = 2
---
+++ frontend/src/components/RegisterVue.vue
@@ -0,0 +1,49 @@
+<template>
+  <form @submit.prevent="handlesubmit">
+    <h3>Sign Up</h3>
+
+    <div class="form-group">
+      <label>Username</label>
+      <input type="text" class="form-control" v-model="Username" placeholder="Username"/>
+    </div>
+
+    <div class="form-group">
+      <label>Password</label>
+      <input type="password" class="form-control" v-model="Password" placeholder="Password"/>
+    </div>
+
+    <div class="form-group">
+      <label>Confirm Password</label>
+      <input type="password" class="form-control" v-model="Password_Confirmation" placeholder="Confirm Password"/>
+    </div>
+
+    <button class="btn btn-primary btn-block">Register</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'RegisterVue',
+  data () {
+    return {
+      Username: '',
+      Password: '',
+      Password_Confirmation: ''
+    }
+  },
+  methods: {
+    async handlesubmit () {
+      const data = {
+        username: this.Username,
+        password: this.Password,
+        password_Confirmation: this.Password_Confirmation
+      }
+      console.log(data)
+      const response = await axios.post('/register', data)
+      console.log(response)
+    }
+  }
+}
+</script>
